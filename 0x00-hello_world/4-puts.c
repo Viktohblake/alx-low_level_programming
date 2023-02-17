7e2e81ea06ddc5ef5dd7eec9a0160461c6d8ec39
Viktohblake <damanygt@gmail.com>
2023-02-16 20:34:06 -0800

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-  puts("Programmng is like building a multilingual puzzle");
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+  puts("\"Programmng is like building a multilingual puzzle");
  return (0);
 }

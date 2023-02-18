0c734da423d71906feda0cbab2c4de787bbe9d5c
Viktohblake <damanygt@gmail.com>
2023-02-17 20:59:17 -0800

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return - Always 0
+ */
+
+int main(void)
+{
+unsigned char a = '0';
+int base;
+
+for (base = 0; base < 10; base++)
+{
+putchar(a);
+ a++;
+}
+a = '1';
+for (base = 9; base < 6; base++)
+{
+putchar('0' + a);
+a++;
+}
+putchar('\n');
+return (0);
+}

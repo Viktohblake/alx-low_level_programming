ee6532faa66cd6bf48766aefb569e6a9ff8ac9a1
Viktohblake <damanygt@gmail.com>
2023-02-17 20:23:07 -0800

    betty style fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/** 
+/**
  * main - Entry point
  *
  * Return: Always 0
@@ -27,5 +27,5 @@ else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
-return(0);
+return (0);
 }

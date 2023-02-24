787f4fd58b29f715df7dceb1c1239571547e650d
Viktohblake <damanygt@gmail.com>
2023-02-23 19:23:32 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/2-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_numbers - function that print numbers from 0 - 9
+*/
+int print_numbers(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+_putchar(c);
+_putchar('\n');
+}
